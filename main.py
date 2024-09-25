7c019fa75c97d6a4e440bf5fb57e8c1675f09cf7
galahadtrail <zzzzrzzzz@yandex.ru>
2024-09-25 16:55:08 +0300

    finished report function

--- main.py
+++ main.py
@@ -27,6 +27,16 @@ def characters_amount(content):
 def report_make(path, amount_ch, ch_dict):
 	filename = basename(path)
 	print(Fore.GREEN + "--- Begin report of " + filename + " ---")
+	print(Fore.RED + str(amount_ch) + Style.RESET_ALL + " words was found in the document")
+	print()
+	ch_dict = dict(sorted(ch_dict.items(), key=lambda x:x[1], reverse=True))
+
+	for key, value in ch_dict.items():
+		print("The " + Fore.RED + key + " character was found " + str(value) + " times")
+
+	print(Fore.GREEN + "--- End report ---")
+
+
 
 if __name__ == '__main__':
 	path = "books/frankenstein.txt"
