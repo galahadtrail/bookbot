a0903eec2dd342e52542410bb2bcb7c25147798b
galahadtrail <zzzzrzzzz@yandex.ru>
2024-09-25 15:56:49 +0300

    done with a few functions

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-with open("books/frankenstein.txt", "r") as file:
-	print(file.read())
+def read_book(path):
+	with open("books/frankenstein.txt", "r") as file:
+		return file.read()
+
+def words_amount(content):
+	content = content.split(" ")
+	return len(content)
+
+
+if __name__ == '__main__':
+	content = read_book("books/frankenstein.txt")
+	amount = words_amount(content)
+
+	print(amount)
+	
